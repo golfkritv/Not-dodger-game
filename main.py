a780e4d88bfcdb8f5e62149532040cec3d40b857
golfkritv <krittipat570@gmail.com>
2021-02-18 10:15:09 +0700

    New setup of the code, simpler and easier to understand

--- main.py
+++ main.py
@@ -1,40 +1,35 @@
 # gain access to pygame in our code
 import pygame
-# initialize all pygame modules
-pygame.init()
+import random
 
 # create game window
-screen = pygame.display.set_mode((500, 500))
+screen = pygame.display.set_mode((600, 600))
 pygame.display.set_caption("Not Dodger game")
 
+#instruction of the game
+FPS = 60
+
+ENEMY_SPAWN_RATE = 2
+ENEMY_MIN_SIZE = 5
+ENEMY_MAX_SIZE = 20
+ENEMY_MIN_SPEED = 4
+ENEMY_MAX_SPEED = 15
+
+PLAYER_SPEED = 5
+PLAYER_SIZE = 12
+PLAYER_MAX_UP = 100
+
+BACKGROUND_COLOR = pygame.Color("black")
+TEXT_COLOR = pygame.Color("white")
+ENEMY_COLOR = pygame.Color("darkgreen")
+PLAYER_COLOR = pygame.Colour("darkred")
+
+
 # game loop
-gameRunning = True
-while gameRunning:
+def run():
+    pass
 
-    # fill pur screen wth a colour
 
-    FPS = 60
 
-    ENEMY_SPAWN_RATE = 2
-    ENEMY_MIN_SIZE = 5
-    ENEMY_MAX_SIZE = 20
-    ENEMY_MIN_SPEED = 4
-    ENEMY_MAX_SPEED = 15
-
-    PLAYER_SPEED = 5
-    PLAYER_SIZE = 12
-    PLAYER_MAX_UP = 100
-
-    BACKGROUND_COLOR = pygame.Color("black")
-    TEXT_COLOR = pygame.Color("white")
-    ENEMY_COLOR = pygame.Color("darkgreen")
-    PLAYER_COLOR = pygame.Colour("darkred")
-
-    # event loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            gameRunning = False
-
-    # update game when action occur
-    pygame.display.update()
-
+if __name__ = '__main__'
+    run()
