592fdad4bf4ad5000622014aa41a140af19e480c
golfkritv <krittipat570@gmail.com>
2021-02-19 09:09:39 +0700

    finished set up

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 # gain access to pygame in our code
+import pip
 import pygame
 import random
 
-# create game window
-screen = pygame.display.set_mode((600, 600))
-pygame.display.set_caption("Not Dodger game")
-
-#instruction of the game
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+# instruction of the game
 FPS = 60
 
 ENEMY_SPAWN_RATE = 2
@@ -17,7 +16,7 @@ ENEMY_MAX_SPEED = 15
 
 PLAYER_SPEED = 5
 PLAYER_SIZE = 12
-PLAYER_MAX_UP = 100
+PLAYER_MAX_UP = 150
 
 BACKGROUND_COLOR = pygame.Color("black")
 TEXT_COLOR = pygame.Color("white")
@@ -27,9 +26,35 @@ PLAYER_COLOR = pygame.Colour("darkred")
 
 # game loop
 def run():
-    pass
+    pygame.init()
+
+    # create game window
+    Clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set.caption("Not dodger game")
+
+    surface = pygame.Surface(screen.get_size())
+    surface = surface.convert()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.OUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K.ESCAPE:
+                running = False
+
+        clock.tick(FPS)
+
+        surface.fill(BLACKGROUND_COLOR)
+
+        screen.blit(surface, (0, 0))
+
+        pygame.display.update()
 
 
 
-if __name__ = '__main__'
+
+if __name__ == '__main__':
     run()
+    pygame.quit()
